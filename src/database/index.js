ec8e1c1269c95ae6af434ed50b6f665d9d429e87
Bruno Ribeiro <brunomestres@gmail.com>
2020-04-22 00:27:21 -0300

    Modulo de agendamento

--- src/database/index.js
+++ src/database/index.js
@@ -2,7 +2,9 @@ import Sequelize from 'sequelize';
 import databaseConfig from '../config/database';
 import User from '../app/models/User';
 import File from '../app/models/File';
-const models = [User , File];
+import Appointments from '../app/models/Appointments';
+
+const models = [User, File, Appointments];
 class Database {
     constructor() {
         this.init();
@@ -13,7 +15,10 @@ class Database {
 
         models
             .map((model) => model.init(this.connection))
-            .map(model => model.associate && model.associate(this.connection.models))
+            .map(
+                (model) =>
+                    model.associate && model.associate(this.connection.models)
+            );
     }
 }
 
--- src/database/migrations/20200414170345-add-field-to-user.js
+++ src/database/migrations/20200414170345-add-field-to-user.js
@@ -1,27 +1,18 @@
-'use strict';
-
 module.exports = {
-  up: (queryInterface, Sequelize) => {
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.addColumn('users', 'avatar_id', {
+            type: Sequelize.INTEGER,
+            references: {
+                model: 'files',
+                key: 'id',
+            },
+            onUpdate: 'CASCADE',
+            onDelete: 'SET NULL',
+            allowNull: true,
+        });
+    },
 
-      return queryInterface.addColumn(
-          'users',
-          'avatar_id',
-          {
-              type:Sequelize.INTEGER,
-              references:{
-                  model: 'files',
-                  key: 'id',
-                },
-                onUpdate: 'CASCADE',
-                onDelete: 'SET NULL',
-                allowNull: true
-          }
-      )
-  },
-
-  down: (queryInterface) => {
-
-      return queryInterface.removeColumn('users','avatar_id');
-
-  }
+    down: (queryInterface) => {
+        return queryInterface.removeColumn('users', 'avatar_id');
+    },
 };
